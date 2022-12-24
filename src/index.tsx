6e714348ba353930acbda0f031c422ce7af8e611
Zan Cerne <mail@cernezan.com>
2022-12-24 17:03:50 +0100

    Open in browser action, word count

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import { ActionPanel, Detail, List, Action } from "@raycast/api";
+import { ActionPanel, Detail, List, openCommandPreferences, getPreferenceValues, Action, showToast, Toast, popToRoot } from "@raycast/api";
 import { useEffect, useState } from "react";
+import { Actions } from "./Actions";
 import { getQueue } from "./matterApi";
-
 
 interface State {
   items?: any;
@@ -9,16 +9,23 @@ interface State {
 }
 
 export default function Command() {
-
-
   const [state, setState] = useState<State>({})
+  const [loading, setLoading] = useState<boolean>(true)
 
   useEffect(() => {
     async function fetchQueue() {
+      setLoading(true)
       try {
-        const items = await getQueue()
+        const items: any = await getQueue()
+        if (items.code == 'token_not_valid') {
+          showToast(Toast.Style.Failure, "Token not valid", "Please check your token in preferences")
+          popToRoot()
+          return
+        }
         setState({ items })
+        setLoading(false)
       } catch (error) {
+        setLoading(false)
         setState({
           error: error instanceof Error ? error : new Error('Something went wrong'),
         })
@@ -28,14 +35,28 @@ export default function Command() {
     fetchQueue()
   }, [])
 
-  console.log(state, "HEREE")
 
   return (
-    <List isLoading={!state}>
-      {state.items?.feed.map((item: any) => (
-        <List.Item title={item.content.title} />
-      ))}
+    <>
+      <List isLoading={loading}>
+        {state.items?.feed.map((item: any) => (
+          <List.Item
+            key={item.id}
+            icon={item.content.photo_thumbnail_url}
+            title={item.content.title}
+            actions={<Actions item={item} />}
+            accessories={[{
+              text: item.content.article?.word_count ? item.content.article?.word_count.toString() + ' words' : ''
+            }]}
+          />
 
-    </List>
+        ))}
+
+      </List>
+
+
+    </>
+
+
   );
 }
--- src/matterApi.ts
+++ src/matterApi.ts
@@ -1,19 +1,23 @@
 import fetch, { Headers } from "node-fetch";
+import { getPreferenceValues } from "@raycast/api";
+import { Preferences } from "./types";
 
 
 const url = 'https://web.getmatter.com/api/library_items/queue_feed?page=1'
-const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjcyMzQxOTA1LCJqdGkiOiIzYzM4Y2U3MTYzMWI0NDY4YTE0OGI5Y2MyYjYwODRhYyIsInVzZXJfaWQiOjgzNTQzLCJjbGllbnRfdHlwZSI6ImFwcCIsImxvZ2luX3Nlc3Npb25faWQiOiJjNDg0YzUyYS1jOTQ0LTRlNmUtOGFlZi0wZWQwZjkzNTIxOWYifQ.McDI_M36MqiBpntucWbjB5t6zoHdi7oDhk2CdwMPBTE'
+// const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjcyMzQxOTA1LCJqdGkiOiIzYzM4Y2U3MTYzMWI0NDY4YTE0OGI5Y2MyYjYwODRhYyIsInVzZXJfaWQiOjgzNTQzLCJjbGllbnRfdHlwZSI6ImFwcCIsImxvZ2luX3Nlc3Npb25faWQiOiJjNDg0YzUyYS1jOTQ0LTRlNmUtOGFlZi0wZWQwZjkzNTIxOWYifQ.McDI_M36MqiBpntucWbjB5t6zoHdi7oDhk2CdwMPBTE'
 
-const options = {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
 
-    }
-}
 
 async function getQueue() {
+    const token = getPreferenceValues<Preferences>().matterToken
+    const options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+
+        }
+    }
 
     try {
         const response = await fetch(url, options)
@@ -21,7 +25,6 @@ async function getQueue() {
     } catch (error) {
         console.log(error)
     }
-
 }
 
 export { getQueue }
